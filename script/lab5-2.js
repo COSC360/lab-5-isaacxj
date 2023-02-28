bb2c9598c45198fddb8b906c559ecbfcb6883bc1
isaacxj <isaacjoseph2001@gmail.com>
2023-02-27 20:13:17 -0800

    Update lab5-2.js

--- script/lab5-2.js
+++ script/lab5-2.js
@@ -1,21 +1,19 @@
-window.onload = function(){
-    var elements = document.getElementsByTagName("*");
-    
-    for (var i = 1; i < elements.length; i++){
-        if (elements[i].nodeType == 1){
-            var txt = elements[i].tagName;
-            var newNode = document.createTextNode(txt)
-            elements[i].appendChild(newNode);
-
-            elements[i].classList.add(".hoverNode");
-            elements[i].className = "hoverNode";
-            
-            elements[i].style.backgroundColor = "yellow";
-
-            elements[i].addEventListener("click", function(){
-                alert("Tag Name: " + elements[i].tagName + " innerHTML: " + elements[i].innerHTML);
-            })
-           
-        }
+window.addEventListener('load', () => {
+    var nodes = [...document.body.getElementsByTagName('*')];
+    var parentInfo = [];
+    var newNodes = [];
+  
+    for (i = 0; i < nodes.length; i++) {
+      if (nodes[i].nodeType === 3) {
+        console.log(nodes[i].nodeName, nodes[i].nodeType)
+      } else {
+        newNodes[i] = nodes[i].appendChild(document.createElement('button'));
+        newNodes[i].classList.add('hoverNode');
+        parentInfo[i] = newNodes.innerHTML || nodes[i]?.innerHTML;
+        newNodes[i].innerHTML = nodes[i].parentElement.tagName;
+        newNodes[i].addEventListener('click', () => {
+          window.alert('ID: ' + i.toString() + ", innerHTML: " + parentInfo[i]);
+        })
+      }
     }
-}
+  })
